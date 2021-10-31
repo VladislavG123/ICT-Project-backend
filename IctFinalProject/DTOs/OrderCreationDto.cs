fbb00ddf152cb486e7826fad9d338497b654c1a1
vladislavg123 <gvo_step2018@mail.ru>
2021-10-31 14:03:59 +0600

    DeliveryTime Format

--- IctFinalProject/DTOs/OrderCreationDto.cs
+++ IctFinalProject/DTOs/OrderCreationDto.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IctFinalProject.DTOs
 {
     public class OrderCreationDto
     {
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{MM/dd/yyyy HH:mm:ss}")]
         public DateTime DeliveryTime { get; set; }
         public string PhoneNumber { get; set; }
     }
